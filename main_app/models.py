6b3b8bbf3d1b919d1213a23edeb68172ada1db3a
michellejtan <michelle0223@gmail.com>
2025-04-26 16:37:58 -0700

    create a Plant model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    # Many-to-Many relationship with supplies
+    # supplies = models.ManyToManyField(Supply)
+
+    def __str__(self):
+        return self.name
