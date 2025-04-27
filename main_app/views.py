46fe2fec25f95af033de92c3a1f051e11f4bc166
michellejtan <michelle0223@gmail.com>
2025-04-27 16:30:08 -0700

    wip: import the Plant model, &  create the home & cat_index view function

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .models import Plant
 # Create your views here.
+# Import HttpResponse to send text-based responses
+# from django.http import HttpResponse
+
+# Define the home view function
+def home(request):
+    # Send a simple HTML response
+    # return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+    return render(request, 'home.html')
+
+
+def plant_index(request):
+    plants = Plant.objects.all()
+    return render(request, 'plants/index.html', {
+        'plants': plants
+    })
