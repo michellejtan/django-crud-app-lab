69200a087735e74538080f8629b1e01237effe5b
michellejtan <michelle0223@gmail.com>
2025-04-27 16:26:19 -0700

    wip: define the plant index route

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [
     # Routes will be added here
     path('', views.home, name='home'),
+    path('plants/', views.plant_index, name='plant-index'),
+
 ]
