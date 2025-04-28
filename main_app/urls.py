880861769117026905dd9717d056db4350349901
michellejtan <michelle0223@gmail.com>
2025-04-28 15:28:24 -0700

    wip: add two new routes for update and delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('plants/', views.plant_index, name='plant-index'),
     path('plants/<int:plant_id>/', views.plant_detail, name='plant-detail'), # can change the link address
     path('plants/create/', views.PlantCreate.as_view(), name='plant-create'),
+    path('plants/<int:pk>/update/', views.PlantUpdate.as_view(), name='plant-update'),
+    path('plants/<int:pk>/delete/', views.PlantDelete.as_view(), name='plant-delete'),
 ]
