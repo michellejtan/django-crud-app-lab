88a4064dcfd06b34f560e65cad74373f32bdc74f
michellejtan <michelle0223@gmail.com>
2025-04-28 10:54:31 -0700

    wip:  Define the plant detail route

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # Routes will be added here
     path('', views.home, name='home'),
     path('plants/', views.plant_index, name='plant-index'),
+    path('plants/<int:plant_id>/', views.plant_detail, name='plant-detail'),
 
 ]
