ec90d124edb11c42aa8261024ebf5ce87ae84937
michellejtan <michelle0223@gmail.com>
2025-04-28 13:34:03 -0700

    wip: add the route used to create a cat

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Routes will be added here
     path('', views.home, name='home'),
     path('plants/', views.plant_index, name='plant-index'),
-    path('plants/<int:plant_id>/', views.plant_detail, name='plant-detail'),
-
+    path('plants/<int:plant_id>/', views.plant_detail, name='plant-detail'), # can change the link address
+    path('plants/create/', views.PlantCreate.as_view(), name='plant-create'),
 ]
