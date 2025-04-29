aa5238d57df2c5fbeb68f8ea9ed010bc301ec2b4
michellejtan <michelle0223@gmail.com>
2025-04-28 21:52:24 -0700

    wip: import and register the Care model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Plant
+from .models import Plant, Care
 # Register your models here.
-admin.site.register(Plant)
+admin.site.register([Plant, Care])
