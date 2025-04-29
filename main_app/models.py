6988c89d0a4b92cf65329e4d9494153cffc23472
michellejtan <michelle0223@gmail.com>
2025-04-28 21:31:24 -0700

    wip: Add a new Care Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.urls import reverse
 
+# A tuple of 2-tuples added above our models
+CARE_TIMES = (
+    ('M', 'Morning'),
+    ('A', 'Afternoon'),
+    ('E', 'Evening')
+)
+#^the value that will be stored in the database,  the human-friendly “display” value
+
 # Create your models here.
 class Plant(models.Model):
     name = models.CharField(max_length=100)
@@ -17,3 +25,19 @@ class Plant(models.Model):
     def get_absolute_url(self):
         # Use the 'reverse' function to dynamically find the URL for viewing this plant's details
         return reverse('plant-detail', kwargs={'plant_id': self.id})
+    
+class Care(models.Model):
+    date = models.DateField('Care date')
+    time_of_day = models.CharField(
+        max_length=1,
+        choices=CARE_TIMES,
+        default=CARE_TIMES[0][0] #M for morning
+    )
+
+    # Connect each care entry to a plant
+    # Create a plant_id column for each care in the database
+    #care belongs to a Plant
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    #^one-ton-many relationship, if a Plant record is deleted, all the child Care will be deleted
+    def __str__(self):
+        return f"{self.get_time_of_day_display()} on {self.date}"    
