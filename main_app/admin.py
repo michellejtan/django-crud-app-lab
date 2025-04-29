ef50ce164ebc6537848f24e82af01dee50aa6362
michellejtan <michelle0223@gmail.com>
2025-04-29 15:07:23 -0700

    wip: import and register the Supply model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Plant, Care
+from .models import Plant, Care, Supply
 # Register your models here.
-admin.site.register([Plant, Care])
+admin.site.register([Plant, Care, Supply])
