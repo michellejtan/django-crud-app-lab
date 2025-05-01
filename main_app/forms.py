7f0cd863ce3d667abc2eaf877bd240e7b81d24f6
michellejtan <michelle0223@gmail.com>
2025-05-01 14:49:43 -0700

    wip: create CustomUserCreationForm, email as optional

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 from .models import Care
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']  # email is optional unless you require it
+
 
 class CareForm(forms.ModelForm):
     class Meta:
