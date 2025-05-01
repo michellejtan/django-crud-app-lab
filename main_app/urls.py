524149a2a64500f084dcdd8751a857c298a2f35f
michellejtan <michelle0223@gmail.com>
2025-05-01 11:52:36 -0700

    wip: home path to use the new home CBV

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,8 @@ from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
     # Routes will be added here
-    path('', views.home, name='home'),
+    # path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('plants/', views.plant_index, name='plant-index'),
     path('plants/<int:plant_id>/', views.plant_detail, name='plant-detail'), # can change the link address
     path('plants/create/', views.PlantCreate.as_view(), name='plant-create'),
